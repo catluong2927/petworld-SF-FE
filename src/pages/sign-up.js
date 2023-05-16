e23d25a43c6684fc80e1dc772a17a98c79a261ea
itachi2927 <nguyengiacatluong92@gmail.com>
2023-05-16 22:45:05 +0700

    feature login + register

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import {json, Link} from "react-router-dom";
 import React, {useRef, useState} from "react";
 import Breadcrumb from "../components/breadcrumb/Breadcrumb";
 import Layout from "../layout/Layout";
@@ -12,12 +12,39 @@ import axios from "axios";
 function SignUpPage() {
     const toast = useRef(null);
     const LOGIN_API = process.env.REACT_APP_FETCH_API;
+    const [isCheckedCus, setIsCheckedCus] = useState(true);
+    const [isCheckedOwn, setIsCheckedOwn] = useState(false);
     const [form, setForm] = useState({});
+    const [messErorr,setMessErorr] = useState({})
     const REGEX = {
         email: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
         password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
         username: /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
     };
+
+    const  checkBoxCusHandler = () => {
+        setIsCheckedCus(!isCheckedCus)
+    }
+    const  checkBoxHandOwnHandler = () => {
+        setIsCheckedOwn(!isCheckedOwn)
+    }
+    let roles = [];
+    if (isCheckedCus) {
+        const customer = {
+            "id":2,
+            "name":"ROLE_CUSTOMER",
+            "desc":"Khách hàng"
+        }
+        roles.push(customer);
+    }
+    if (isCheckedOwn) {
+        const owner = {
+            "id":3,
+            "name":"ROLE_OWNER",
+            "desc":"Trung tâm dịch vụ"
+        }
+        roles.push(owner);
+    }
     function handleChangeSignup(event) {
         setForm({
             ...form,
@@ -38,22 +65,43 @@ function SignUpPage() {
         if (!form.username) {
             errors.username = "Required";
         }
+        if (!form.confirmPassword) {
+            errors.confirmPassword = "Required";
+        }
         return errors;
     }
 
     function handleSubmit() {
+        const data ={
+            userName: form.username,
+            fullName: form.fullName,
+            email: form.email,
+            password:form.password,
+            roles
+        }
         axios
-            .post(`${LOGIN_API}/users`, form)
+            .post(`${LOGIN_API}/users`, data)
             .then((res) => {
-                toast.current.show({severity: 'success', summary: 'Success', detail: 'Message Content', life: 3000})
-                setTimeout(window.location.href = "/", 6000)
+                console.log(res)
+                if (res.data === '') {
+                    toast.current.show({severity: 'success', summary: 'Success', detail: 'registration successful', life: 3000})
+                    setTimeout(() => { window.location.href = "/"; }, 6000)
+                } else {
+                    setMessErorr(res.data.userName);
+                    toast.current.show(
+                        {severity: 'error', summary: 'Error', detail: 'registration fail', life: 3150})
+                }
             })
             .catch(err => {
-                    toast.current.show(
-                        {severity: 'error', summary: 'Error', detail: 'Message Content', life: 3150})
-                }
-            )
+                console.log(err.message + "abca");
+                toast.current.show(
+                    {severity: 'error', summary: 'Error', detail: 'registration fail', life: 3150})
+            });
     }
+
+
+
+
 
     return (
         <>
@@ -150,7 +198,7 @@ function SignUpPage() {
                                                             <input
                                                                 type="password"
                                                                 name="confirmPassword"
-                                                                id="password"
+                                                                id="confirmpassword"
                                                                 placeholder="Enter Your Password"
                                                                 value={form.confirmPassword || ""}
                                                                 onChange={handleChangeSignup}
@@ -162,17 +210,15 @@ function SignUpPage() {
                                                     <div className="col-md-12">
                                                         <div
                                                             className="form-agreement form-inner d-flex justify-content-between flex-wrap">
-                                                            <div className="form-group">
-                                                                <input type="checkbox" id="html" value="customer"/>
-                                                                <label htmlFor="html1">
-                                                                    Customer
-                                                                </label>
+                                                            <div className="form-group" onClick={checkBoxCusHandler}>
+                                                                <input defaultChecked="checked" type="checkbox" id="customer" value="customer" />
+                                                                <p className="sign-up-checkbox" >{isCheckedCus && <p className="sign-up-checkbox--checked" ></p>}</p>
+                                                                <label htmlFor="customer">Customer</label>
                                                             </div>
-                                                            <div className="form-group">
-                                                                <input type="checkbox" id="html" value="Owner"/>
-                                                                <label htmlFor="html2">
-                                                                    Owner
-                                                                </label>
+                                                            <div className="form-group" onClick={checkBoxHandOwnHandler}>
+                                                                <input  type="checkbox" id="owner" value="owner"/>
+                                                                <p className="sign-up-checkbox" >{isCheckedOwn && <p className="sign-up-checkbox--checked" ></p>}</p>
+                                                                <label htmlFor="owner">Owner</label>
                                                             </div>
                                                         </div>
                                                     </div>
