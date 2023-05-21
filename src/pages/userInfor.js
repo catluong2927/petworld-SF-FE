7887eff391671dea7732ae898f90ee11bf5e6528
itachi2927 <nguyengiacatluong92@gmail.com>
2023-05-21 14:09:33 +0700

    fix issue login css

--- src/pages/userInfor.js
+++ src/pages/userInfor.js
@@ -1,22 +1,34 @@
 import {Link} from "react-router-dom";
+import React from "react";
+import {useSelector} from "react-redux";
 export const  UserInfor = ()=>{
+    const isLogin = useSelector((state) => state.auth.login?.currentUser);
     return(
         <div className='info'>
-            {/*<div className="header-infor">*/}
-            {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN7v4Q4NzyvPUeP2yNEtp7xXzeyOoKgpWTOw&usqp=CAU" alt="" className="header-infor-image"/>*/}
-            {/*    <h1 className="header-infor-name">Nguyen Van Minh</h1>*/}
-            {/*</div>*/}
-            <div>
+            <li>
+            <p className='infor-linkusername'>Hi! {isLogin.userDtoResponse.userName}</p>
+            </li>
+            <li>
+            <p className='infor-linkusername'>{isLogin.userDtoResponse.email}</p>
+            </li>
+            <hr/>
+            <ul className= 'inforuserul'>
+                <li className= 'inforuserli'>
                 <Link to='/profile' className='none-border-circle'>
                     <span className='infor-link'>Profile</span>
                 </Link>
+                </li>
+                <li className= 'inforuserli'>
                 <Link to='/profile' className='none-border-circle'>
                     <span className='infor-link'>Settings</span>
                 </Link>
+                </li>
+                <li className= 'inforuserli'>
                 <Link to='/profile' className='none-border-circle'>
                     <span className='infor-link'>Logout</span>
                 </Link>
-            </div>
+                </li>
+            </ul>
             <div className="footer-infor">
             </div>
         </div>
